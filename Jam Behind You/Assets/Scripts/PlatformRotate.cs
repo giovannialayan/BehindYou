97f029bf7c6f8e76cf91b5940d487c6192ca51c6
Jack Furmanek <jackfu155@gmail.com>
2019-11-02 10:36:27 -0400

    sync

---
+++ Jam Behind You/Assets/Scripts/PlatformRotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformRotate : MonoBehaviour
+{
+    public bool rotate = true;
+    public float RX;
+    public float RY;
+    public float RZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rotate == true)
+        {
+            transform.Rotate(RX, RY, RZ);
+        }
+        
+    }
+}
