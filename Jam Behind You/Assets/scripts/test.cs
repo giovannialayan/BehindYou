43ddbabc0a50f5bcee5983bdb9251ae24555c38c
giovannialayan <giovannialayan@gmail.com>
2019-11-01 22:08:35 -0400

    edited gioTest scene

---
+++ Jam Behind You/Assets/scripts/test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    //player variables
+    private Rigidbody playerRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
