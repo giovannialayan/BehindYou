5a2da4bd9489a38e21a384f8b60382cfec3ea536
giovannialayan <giovannialayan@gmail.com>
2019-11-02 09:59:54 -0400

    fixed platform moving up and down errors and added ability to change size of platforms

--- Jam Behind You/Assets/Scripts/PlatformMove.cs
+++ Jam Behind You/Assets/Scripts/PlatformMove.cs
@@ -4,31 +4,85 @@ using UnityEngine;
 
 public class PlatformMove : MonoBehaviour
 {
+    //basic movement variables
     public List<GameObject> points;
     public Vector3 pos;
     public float speed = 1.0f;
     private float startTime;
     private float journeyLength;
+
+    //size change variables
+    private Vector3 originalSize;
+
+    //platform type to be set in the inspector
+    public bool basicMovement;
+    public bool changeSize;
+
+    //platform control
+    private bool goingBackwards = false;
+
     // Start is called before the first frame update
     void Start()
     {
         startTime = Time.time;
+        originalSize = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        for(int i = 0; i < points.Count; i++)
+        //handle moving from point A to point B
+        if (basicMovement)
         {
-            if(i <= points.Count - 1)
+            //for (int i = 0; i < points.Count; i++)
+            //{
+            //    if (i <= points.Count - 1)
+            //    {
+            //        journeyLength = Vector3.Distance(points[i].transform.position, points[i + 1].transform.position);
+
+            //        float distCovered = (Time.time - startTime) * speed;
+
+            //        float fractionOfJourney = distCovered / journeyLength;
+
+            //        transform.position = Vector3.Lerp(points[i].transform.position, points[i + 1].transform.position, fractionOfJourney);
+            //    }
+            //}
+
+            if (Vector3.Distance(transform.position, points[1].transform.position) > 0 && !goingBackwards)
             {
-                journeyLength = Vector3.Distance(points[i].transform.position, points[i + 1].transform.position);
+                transform.position = Vector3.MoveTowards(transform.position, points[1].transform.position, speed * Time.deltaTime);
+            }
+            else if(Vector3.Distance(transform.position, points[0].transform.position) > 0 && goingBackwards)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, points[0].transform.position, speed * Time.deltaTime);
+            }
+            else if (!goingBackwards)
+            {
+                goingBackwards = true;
+            }
+            else
+            {
+                goingBackwards = false;
+            }
+        }
 
-                float distCovered = (Time.time - startTime) * speed;
-
-                float fractionOfJourney = distCovered / journeyLength;
-
-                transform.position = Vector3.Lerp(points[i].transform.position, points[i + 1].transform.position, fractionOfJourney);
+        if (changeSize)
+        {
+            if(Vector3.Distance(transform.localScale, originalSize * .5f) > 0 && !goingBackwards)
+            {
+                transform.localScale = Vector3.MoveTowards(transform.localScale, originalSize * .5f, speed * Time.deltaTime);
+            }
+            else if(Vector3.Distance(transform.localScale, originalSize) > 0 && goingBackwards)
+            {
+                transform.localScale = Vector3.MoveTowards(transform.localScale, originalSize, speed * Time.deltaTime);
+            }
+            else if(!goingBackwards)
+            {
+                goingBackwards = true;
+            }
+            else
+            {
+                goingBackwards = false;
             }
         }
     }
