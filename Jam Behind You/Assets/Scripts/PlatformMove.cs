1f0699e2ddbec4b7e3421af181dc42e28409e6aa
Jack Furmanek <jackfu155@gmail.com>
2019-11-01 22:38:59 -0400

    sync

---
+++ Jam Behind You/Assets/Scripts/PlatformMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMove : MonoBehaviour
+{
+    public List<GameObject> points;
+    public Vector3 pos;
+    public float speed = 1.0f;
+    private float startTime;
+    private float journeyLength;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < points.Count; i++)
+        {
+            if(i <= points.Count - 1)
+            {
+                journeyLength = Vector3.Distance(points[i].transform.position, points[i + 1].transform.position);
+
+                float distCovered = (Time.time - startTime) * speed;
+
+                float fractionOfJourney = distCovered / journeyLength;
+
+                transform.position = Vector3.Lerp(points[i].transform.position, points[i + 1].transform.position, fractionOfJourney);
+            }
+        }
+    }
+}
